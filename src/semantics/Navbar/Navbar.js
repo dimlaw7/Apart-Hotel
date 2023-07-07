f299582e07371d0e923d84935928d016c55ffa53
dimlaw7 <dimlaw7@gmail.com>
2023-07-07 16:47:14 +0100

    Add Header and contact information

--- src/semantics/Navbar/Navbar.js
+++ src/semantics/Navbar/Navbar.js
@@ -7,53 +7,93 @@ const Navbar = () => {
     const openMobileMenu = () => setClick(!click)
     const closeMobileMenu = () => setClick(false)
   return (
-    <nav className='navbar'>
-        <div className='container flex_space'>
-            <div className='menu-icon'  onClick={openMobileMenu}>
-                <i className={ click ? 'fas fa-times' : 'fas fa-bars'}></i>
+    <>
+        <nav className='navbar'>
+            <div className='container flex_space'>
+                <div className='menu-icon'  onClick={openMobileMenu}>
+                    <i className={ click ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li>
+                        <Link to='/' onClick={closeMobileMenu}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/home' onClick={closeMobileMenu}>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/gallery' onClick={closeMobileMenu}>Gallery</Link>
+                    </li>
+                    <li>
+                        <Link to='/destination' onClick={closeMobileMenu}>Destination</Link>
+                    </li>
+                    <li>
+                        <Link to='/blog' onClick={closeMobileMenu}>Blog</Link>
+                    </li>
+                    <li>
+                        <Link to='/testimonial' onClick={closeMobileMenu}>Testimonial</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact' onClick={closeMobileMenu}>Contact</Link>
+                    </li>
+                </ul>
+                <div className='login-area flex'>
+                    <li>
+                        <Link to='/sign-in'>
+                            <i className='fa fa-chevron-right'></i>Sign In
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/register'>
+                            <i className='fa fa-chevron-right'></i>Register
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>
+                            <button className='primary-btn'>Request a Quote</button>
+                        </Link>
+                    </li>
+                </div>
             </div>
-            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                <li>
-                    <Link to='/' onClick={closeMobileMenu}>Home</Link>
-                </li>
-                <li>
-                    <Link to='/home' onClick={closeMobileMenu}>About</Link>
-                </li>
-                <li>
-                    <Link to='/gallery' onClick={closeMobileMenu}>Gallery</Link>
-                </li>
-                <li>
-                    <Link to='/destination' onClick={closeMobileMenu}>Destination</Link>
-                </li>
-                <li>
-                    <Link to='/blog' onClick={closeMobileMenu}>Blog</Link>
-                </li>
-                <li>
-                    <Link to='/testimonial' onClick={closeMobileMenu}>Testimonial</Link>
-                </li>
-                <li>
-                    <Link to='/contact' onClick={closeMobileMenu}>Contact</Link>
-                </li>
-            </ul>
-            <div className='login-area flex'>
-                <li>
-                    <Link to='/sign-in'>
-                        <i className='far fa-chevron-right'>Sign In</i>
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/register'>
-                        <i className='far fa-chevron-right'>Register</i>
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/contact'>
-                        <button className='primary-btn'>Request a Quote</button>
-                    </Link>
-                </li>
+        </nav>
+        <header>
+            <div className='container flex_space'>
+                <div className='logo'>
+                    <img src='' alt='logo'></img>
+                </div>
+                <div className='contact flex_space'>
+                    <div class='box flex_space'>
+                        <div className='icons'>
+                            <i className='fa fa-clock'></i>
+                        </div>
+                        <div className='text'>
+                            <h4>Working Hours</h4>
+                            <Link to='/contact'>Monday - Sunday: 9:00am - 6:00pm</Link>
+                        </div>
+                    </div>
+                    <div class='box flex_space'>
+                        <div className='icons'>
+                            <i className='fas fa-phone-volume'></i>
+                        </div>
+                        <div className='text'>
+                            <h4>Call Us</h4>
+                            <Link to='/contact'>+234 901 549 1581</Link>
+                        </div>
+                    </div>
+                    <div class='box flex_space'>
+                        <div className='icons'>
+                            <i className='far fa-envelope'></i>
+                        </div>
+                        <div className='text'>
+                            <h4>Mail Us</h4>
+                            <Link to='/contact'>Dimlaw7@gmail.com</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </nav>
+            
+        </header>
+    </>
+        
   )
 }
 
